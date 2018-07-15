1d4856aeb745896ec6e4caf210dd70a82ed9a2d8
nasitra <nasitra@users.noreply.github.com>
2018-07-15 16:26:06 +0900

    Add 'height' property of 'panel' component

--- js/components/panel.js
+++ js/components/panel.js
@@ -2,8 +2,17 @@
   'use strict';
 
   var jCore = require('jcore');
+  var dom = app.dom || require('../dom.js');
 
-  var Panel = jCore.Component.inherits();
+  var Panel = jCore.Component.inherits(function(props) {
+    this.height = this.prop(props.height);
+  });
+
+  Panel.prototype.onredraw = function() {
+    this.redrawBy('height', function(height) {
+      dom.css(this.element(), { height: height + 'px' });
+    });
+  };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Panel;
