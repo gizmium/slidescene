7060c6bb0b9ed78f2009c31ad7ecfff2587990f4
nasitra <nasitra@users.noreply.github.com>
2018-07-23 19:20:38 +0900

    Make method to load panel in 'content' component

--- js/components/content.js
+++ js/components/content.js
@@ -2,10 +2,18 @@
   'use strict';
 
   var jCore = require('jcore');
+  var Panel = app.Panel || require('./panel.js');
 
   var Content = jCore.Component.inherits(function() {
     this.panels = [];
   });
+
+  Content.prototype.loadPanel = function(props) {
+    var panel = new Panel(props);
+    panel.parentElement(this.element());
+    this.panels.push(panel);
+    return panel.load();
+  };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Content;
