ff04a0077cadb66e3eda47ccf117b4159114a47b
nasitra <nasitra@users.noreply.github.com>
2018-08-12 21:59:10 +0900

    Remove padding of 'panel' component

--- js/components/panel.js
+++ js/components/panel.js
@@ -8,8 +8,6 @@
     this.url = this.prop(props.url);
     this.top = this.prop(props.top);
     this.visible = this.prop(false);
-    this.paddingTop = this.prop(12);
-    this.paddingBottom = this.prop(12);
     this.content = new Panel.Content({ element: this.findElement('.panel-content') });
   });
 
@@ -18,7 +16,7 @@
   };
 
   Panel.prototype.bottom = function() {
-    return this.top() + this.paddingTop() + this.height() + this.paddingBottom();
+    return this.top() + this.height();
   };
 
   Panel.prototype.load = function() {
@@ -38,14 +36,6 @@
 
     this.redrawBy('visible', function(visible) {
       dom.toggleClass(this.element(), 'hide', !visible);
-    });
-
-    this.redrawBy('paddingTop', function(paddingTop) {
-      dom.css(this.element(), { 'padding-top': paddingTop + 'px' });
-    });
-
-    this.redrawBy('paddingBottom', function(paddingBottom) {
-      dom.css(this.element(), { 'padding-bottom': paddingBottom + 'px' });
     });
   };
 
