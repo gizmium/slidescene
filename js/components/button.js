15d27078aa034fe856ec4cb7bdde005c8617d860
nasitra <nasitra@users.noreply.github.com>
2018-08-24 18:21:09 +0900

    Remove 'active' effect of button component

--- js/components/button.js
+++ js/components/button.js
@@ -5,7 +5,6 @@
   var dom = app.dom || require('../dom.js');
 
   var Button = jCore.Component.inherits(function() {
-    this.isActive = this.prop(false);
     this.disabled = this.prop(false);
     this.draggable = new Button.Draggable(this);
   });
@@ -15,10 +14,6 @@
   };
 
   Button.prototype.onredraw = function() {
-    this.redrawBy('isActive', function(isActive) {
-      dom.toggleClass(this.element(), 'active', isActive);
-    });
-
     this.redrawBy('disabled', function(disabled) {
       dom.toggleClass(this.element(), 'disabled', disabled);
     });
@@ -30,15 +25,9 @@
     Draggable.prototype.onstart = function(button, x, y, event, context) {
       context.target = dom.target(event);
       dom.cancel(event);
-      button.isActive(true);
-    };
-
-    Draggable.prototype.onmove = function(button, dx, dy, event, context) {
-      button.isActive(dom.target(event) === context.target);
     };
 
     Draggable.prototype.onend = function(button, dx, dy, event, context) {
-      button.isActive(false);
       if (dom.target(event) === context.target) {
         button.emit('tap');
       }
