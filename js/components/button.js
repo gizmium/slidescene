ce1b7e4b8d18f78b26f2d51ba68163780ce68251
nasitra <nasitra@users.noreply.github.com>
2018-08-29 21:12:36 +0900

    Add 'active' class for button component to redraw precisely in IE

--- js/components/button.js
+++ js/components/button.js
@@ -5,6 +5,7 @@
   var dom = app.dom || require('../dom.js');
 
   var Button = jCore.Component.inherits(function() {
+    this.isActive = this.prop(false);
     this.disabled = this.prop(false);
     this.draggable = new Button.Draggable(this);
   });
@@ -14,6 +15,10 @@
   };
 
   Button.prototype.onredraw = function() {
+    this.redrawBy('isActive', function(isActive) {
+      dom.toggleClass(this.element(), 'active', isActive);
+    });
+
     this.redrawBy('disabled', function(disabled) {
       dom.toggleClass(this.element(), 'disabled', disabled);
     });
@@ -25,9 +30,15 @@
     Draggable.prototype.onstart = function(button, x, y, event, context) {
       context.target = dom.target(event);
       dom.cancel(event);
+      button.isActive(true);
+    };
+
+    Draggable.prototype.onmove = function(button, dx, dy, event, context) {
+      button.isActive(dom.target(event) === context.target);
     };
 
     Draggable.prototype.onend = function(button, dx, dy, event, context) {
+      button.isActive(false);
       if (dom.target(event) === context.target) {
         button.emit('tap');
       }
