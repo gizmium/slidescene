bc512b32d87750b7eb2fe888955a228f53334c62
nasitra <nasitra@users.noreply.github.com>
2018-09-05 18:19:26 +0900

    Modify code for error of loading medal

--- js/components/medal.js
+++ js/components/medal.js
@@ -6,6 +6,7 @@
 
   var Medal = jCore.Component.inherits(function() {
     this.name = this.prop('');
+    this.src = this.prop('');
   });
 
   Medal.prototype.load = function(name) {
@@ -13,14 +14,8 @@
     return new Promise(function(resolve, reject) {
       var children = dom.children(this.element());
       var name = this.name();
-      var src = 'medals/' + name + '.svg';
-      var onfailed = function() {
-        src = Medal.DefaultImageData;
-        dom.attr(children[0], { src: src });
-      };
+      this.src('medals/' + name + '.svg');
       dom.once(children[0], 'load', function() {
-        dom.off(children[0], 'error', onfailed);
-        dom.off(children[0], 'abort', onfailed);
         if (name !== this.name()) {
           return reject();
         }
@@ -29,20 +24,30 @@
             return reject();
           }
           dom.append(this.element(), children[0]);
-          dom.attr(children[1], { src: src });
-          dom.css(this.element(), { 'background-image': 'url("' + src + '")' });
+          dom.attr(children[1], { src: this.src() });
+          dom.css(this.element(), { 'background-image': 'url("' + this.src() + '")' });
           resolve();
         }.bind(this));
         dom.toggleClass(children[0], 'hide', false);
         dom.toggleClass(children[1], 'hide', true);
       }.bind(this));
-      dom.on(children[0], 'error', onfailed);
-      dom.on(children[0], 'abort', onfailed);
-      dom.attr(children[0], { src: src });
+      dom.attr(children[0], { src: this.src() });
     }.bind(this));
   };
 
-  Medal.DefaultImageData = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==';
+  Medal.prototype.oninit = function() {
+    var onfailed = this.onfailed.bind(this);
+    dom.children(this.element()).forEach(function(child) {
+      dom.on(child, 'error', onfailed);
+      dom.on(child, 'abort', onfailed);
+    });
+  };
+
+  Medal.prototype.onfailed = function(event) {
+    var src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==';
+    this.src(src);
+    dom.attr(dom.target(event), { src: this.src() });
+  };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Medal;
